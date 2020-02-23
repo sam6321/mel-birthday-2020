52f4631f91289930acd0e31293f0ee20ecfb6588
sam6321 <sam6321@live.com.au>
2020-02-23 19:08:38 +0800

    Updates

---
+++ precache-manifest.afa21c4538cdf48ffb19001bfac9e055.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "609052bf67bd676c110c414d91504b45",
+    "url": "/mel-birthday-2020/index.html"
+  },
+  {
+    "revision": "3dbbfe2f1f30a35ab875",
+    "url": "/mel-birthday-2020/static/css/main.4a6cd45f.chunk.css"
+  },
+  {
+    "revision": "ecf6cb3b96215c850352",
+    "url": "/mel-birthday-2020/static/js/2.5d2ecd57.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/mel-birthday-2020/static/js/2.5d2ecd57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dbbfe2f1f30a35ab875",
+    "url": "/mel-birthday-2020/static/js/main.1720c6d2.chunk.js"
+  },
+  {
+    "revision": "8bd74585ccfd3833fe6d",
+    "url": "/mel-birthday-2020/static/js/runtime-main.d39719f4.js"
+  }
+]);
